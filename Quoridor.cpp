c25549fc14208bd53ca8f3fa84ab3cc716930f0a
Braden Fiedel <u1307785@utah.edu>
2025-04-07 12:57:27 -0600

    Alter wall representation and determine valid wall moves

--- Quoridor.cpp
+++ Quoridor.cpp
@@ -6,12 +6,10 @@
 const uint8_t BOARD_SIZE = 9;
 const uint8_t WALL_COUNT = 20;
 
-const int8_t DX[] = {0, -1, 0, 1};
-const int8_t DY[] = {-1, 0, 1, 0};
-const uint8_t DOWN_WALL_BITMASK = 0b1110;
-const uint8_t LEFT_WALL_BITMASK = 0b1101;
-const uint8_t UP_WALL_BITMASK = 0b1011;
-const uint8_t RIGHT_WALL_BITMASK = 0b0111;
+const int8_t DX[] = {1, 0, -1, 0};
+const int8_t DY[] = {0, 1, 0, -1};
+const uint8_t VERTICAL_WALL_BITMASK = 0b01;
+const uint8_t HORIZONTAL_WALL_BITMASK = 0b10;
 
 struct GameState {
     std::vector<std::vector<uint8_t>> walls;
@@ -21,33 +19,18 @@ struct GameState {
     uint8_t player2WallCount;
     bool isPlayer1sTurn;
 
-    GameState() : walls(BOARD_SIZE, std::vector<uint8_t>(BOARD_SIZE, 0b1111)), 
+    GameState() : walls(BOARD_SIZE, std::vector<uint8_t>(BOARD_SIZE)), 
                   player1Pos({BOARD_SIZE / 2, 0}),
                   player1WallCount(WALL_COUNT / 2),
                   player2Pos({BOARD_SIZE / 2, BOARD_SIZE - 1}),
                   player2WallCount(WALL_COUNT / 2),
-                  isPlayer1sTurn(true) {
-        for (uint8_t x = 0; x < BOARD_SIZE; x++) {
-            walls[x][0] &= DOWN_WALL_BITMASK;
-            walls[x][BOARD_SIZE - 1] &= UP_WALL_BITMASK;
-        }
-        for (uint8_t y = 0; y < BOARD_SIZE; y++) {
-            walls[0][y] &= LEFT_WALL_BITMASK;
-            walls[BOARD_SIZE - 1][y] &= RIGHT_WALL_BITMASK;
-        }
-    }
+                  isPlayer1sTurn(true) {}
 
     void placeWall(uint8_t x, uint8_t y, uint8_t direction) {
-        if (direction == 0) { // horizontal
-            walls[x][y] &= UP_WALL_BITMASK;
-            walls[x + 1][y] &= UP_WALL_BITMASK;
-            walls[x][y + 1] &= DOWN_WALL_BITMASK;
-            walls[x + 1][y + 1] &= DOWN_WALL_BITMASK;
-        } else { // vertical
-            walls[x][y] &= RIGHT_WALL_BITMASK;
-            walls[x + 1][y] &= LEFT_WALL_BITMASK;
-            walls[x][y + 1] &= RIGHT_WALL_BITMASK;
-            walls[x + 1][y + 1] &= LEFT_WALL_BITMASK;
+        if (direction == 0) {
+            walls[x][y] |= VERTICAL_WALL_BITMASK;
+        } else {
+            walls[x][y] |= HORIZONTAL_WALL_BITMASK;
         }
         if (isPlayer1sTurn) {
             player1WallCount--;
@@ -66,6 +49,20 @@ struct GameState {
         isPlayer1sTurn = !isPlayer1sTurn;
     }
 
+    bool canMoveDirection(uint8_t x, uint8_t y, uint8_t direction) const {
+        switch (direction) {
+            case 0: // Right
+                return !((x == BOARD_SIZE - 1) || (walls[x][y] & 1) || (y > 0 && walls[x][y - 1] & 1));
+            case 1: // Up
+                return !((y == BOARD_SIZE - 1) || ((walls[x][y] >> 1) & 1) || (x > 0 && (walls[x - 1][y] >> 1) & 1));
+            case 2: // Left
+                return !((x == 0) || (walls[x - 1][y] & 1) || (y > 0 && walls[x - 1][y - 1] & 1));
+            case 3: // Down
+                return !((y == 0) || ((walls[x][y - 1] >> 1) & 1) || (x > 0 && (walls[x - 1][y - 1] >> 1) & 1));
+            default: return false;
+        }
+    }
+
     bool canReachGoal(std::pair<uint8_t, uint8_t> playerPos, uint8_t goalY) {
         std::queue<std::pair<uint8_t, uint8_t>> queue;
         std::vector<std::vector<bool>> visited(BOARD_SIZE, std::vector<bool>(BOARD_SIZE, false));
@@ -82,11 +79,10 @@ struct GameState {
                 return true;
             }
 
-            uint8_t cell = walls[x][y];
-            for (int i = 0; i < 4; i++) {
-                if ((cell >> i) & 1) {
-                    int newX = x + DX[i];
-                    int newY = y + DY[i];
+            for (uint8_t i = 0; i < 4; i++) {
+                if (canMoveDirection(x, y, i)) {
+                    uint8_t newX = x + DX[i];
+                    uint8_t newY = y + DY[i];
                     if (!visited[newX][newY]) {
                         queue.push({newX, newY});
                         visited[newX][newY] = true;
@@ -107,23 +103,25 @@ struct GameState {
         std::vector<GameState> validMoves;
 
         std::pair<uint8_t, uint8_t> playerPos = isPlayer1sTurn ? player1Pos : player2Pos;
+        uint8_t x = playerPos.first;
+        uint8_t y = playerPos.second;
         std::pair<uint8_t, uint8_t> otherPlayerPos = isPlayer1sTurn ? player2Pos : player1Pos;
-        for (int i = 0; i < 4; i++) {
-            if ((walls[playerPos.first][playerPos.second] >> i) & 1) {
-                int newX = playerPos.first + DX[i];
-                int newY = playerPos.second + DY[i];
+        for (uint8_t i = 0; i < 4; i++) {
+            if (canMoveDirection(x, y, i)) {
+                uint8_t newX = x + DX[i];
+                uint8_t newY = y + DY[i];
                 if (std::make_pair(newX, newY) == otherPlayerPos) {
-                    if ((walls[newX][newY] >> i) & 1) {
+                    if (canMoveDirection(newX, newY, i)) {
                         GameState newState = *this;
                         newState.movePawn(newX + DX[i], newY + DY[i]);
                         validMoves.push_back(newState);
                     } else {
-                        if ((walls[newX][newY] >> (i + 3 % 4)) & 1) {
+                        if (canMoveDirection(newX, newY, i + 3 % 4)) {
                             GameState newState = *this;
                             newState.movePawn(newX + DX[i + 3 % 4], newY + DY[i + 3 % 4]);
                             validMoves.push_back(newState);
                         }
-                        if ((walls[newX][newY] >> (i + 1 % 4)) & 1) {
+                        if (canMoveDirection(newX, newY, i + 1 % 4)) {
                             GameState newState = *this;
                             newState.movePawn(newX + DX[i + 1 % 4], newY + DY[i + 1 % 4]);
                             validMoves.push_back(newState);
@@ -136,16 +134,24 @@ struct GameState {
                 }
             } 
         }
-        
-        
-        // TODO: Determine wall moves.
-        // for (uint8_t x = 0; x < BOARD_SIZE - 1; x++) {
-        //     for (uint8_t y = 0; y < BOARD_SIZE - 1; y++) {
-        //         if (walls[x][y] >> 2 & 1 && walls[x + 1][y] >> 2 & 1) {
 
-        //         }
-        //     }
-        // }
+        for (uint8_t x = 0; x < BOARD_SIZE - 1; x++) {
+            for (uint8_t y = 0; y < BOARD_SIZE - 1; y++) {
+                if (!((y > 0 && walls[x][y - 1] & 1) || (walls[x][y] & 1) || (walls[x][y + 1] & 1) || ((walls[x][y] >> 1) & 1))) {
+                    GameState newState = *this;
+                    newState.placeWall(x, y, 0);
+                    if (newState.isBoardValid()) {
+                        validMoves.push_back(newState);
+                    }
+                } else if (!((x > 0 && (walls[x - 1][y] >> 1) & 1) || ((walls[x][y] >> 1) & 1) || ((walls[x + 1][y] >> 1) & 1) || (walls[x][y] & 1))) {
+                    GameState newState = *this;
+                    newState.placeWall(x, y, 1);
+                    if (newState.isBoardValid()) {
+                        validMoves.push_back(newState);
+                    }
+                }
+            }
+        }
 
         return validMoves;
     }
